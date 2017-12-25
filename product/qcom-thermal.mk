LineageOS/android_device_nextbit_ether

ether: Move thermal-engine-8992.conf to device tree
Change-Id: I97aafc6d755c8ae34b728f6d41fe9dc5452bd944

product/qcom-thermal.mk
@@ -1,0 +1,2 @@
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal-engine-8992.conf:system/etc/thermal-engine-8992.conf

OsROMS/device_nextbit_ether

product/qcom-thermal.mk
@@ -1,0 +1,2 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine-8992.conf:system/etc/thermal-engine-8992.conf